0b635d34d8e9aec6bdd92ea535f6187a27a65343
Bart de Bruin <bart.fr.debruin@gmail.com>
2015-11-24 10:25:17 +0100

    Updated the ability to run the app on my phone.

---
+++ Toilet Thoughts/Toilet Thoughts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Toilet Thoughts
+//
+//  Created by Bart de Bruin on 24-11-15.
+//  Copyright © 2015 BartandFouad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Toilet Thoughts/Toilet Thoughts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Toilet Thoughts
+//
+//  Created by Bart de Bruin on 24-11-15.
+//  Copyright © 2015 BartandFouad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
