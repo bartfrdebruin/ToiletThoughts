5dd71441715e383829ec0e102bf9b1a9103c3681
Fouad <f.astitou@gmail.com>
2015-11-24 15:37:54 +0100

    Made the connections of the IBoutlets of the ThoughtCustomCell

--- Toilet Thoughts/Toilet Thoughts/ThoughtCustomCell.h
+++ Toilet Thoughts/Toilet Thoughts/ThoughtCustomCell.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ThoughtCustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *thoughtImageThumbnail;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *thoughtLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameThoughtCustomCell;
+@property (weak, nonatomic) IBOutlet UILabel *scoreThoughtCustomCell;
+
 
 @end
