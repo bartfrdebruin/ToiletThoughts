b1d70336b9ca5d1e40ae49da6a16919d7a47c08a
Bart de Bruin <bart.fr.debruin@gmail.com>
2015-12-09 13:58:40 +0100

    Updated SelectedThoughtDC to show the right login user button and to show if a user can up or down vote

--- Toilet Thoughts/Toilet Thoughts/SelectedThoughtDetailVC.m
+++ Toilet Thoughts/Toilet Thoughts/SelectedThoughtDetailVC.m
@@ -32,14 +32,14 @@
     if (currentUser) {
         
         UIButton *userLoginButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
-        [userLoginButton setImage:[UIImage imageNamed:@"person_loggedIn_small"] forState:UIControlStateNormal];
+        [userLoginButton setImage:[UIImage imageNamed:@"profile WHITE ACTIVE"] forState:UIControlStateNormal];
         [userLoginButton addTarget:self action:@selector(goToUserScreen) forControlEvents:UIControlEventTouchUpInside];
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:userLoginButton];
     }
     else {
         
         UIButton *userLoginButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
-        [userLoginButton setImage:[UIImage imageNamed:@"person_small.png"] forState:UIControlStateNormal];
+        [userLoginButton setImage:[UIImage imageNamed:@"profile WHITE PASSIVE"] forState:UIControlStateNormal];
         [userLoginButton addTarget:self action:@selector(goToUserScreen) forControlEvents:UIControlEventTouchUpInside];
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:userLoginButton];
     }
@@ -47,35 +47,43 @@
     UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
                                                                            target:nil action:NULL];
     
-    self.scoreUpButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"thumbsup_small"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreUp)];
-    self.scoreDownButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"thumbsdown_small"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreDown)];
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *savedVote = [defaults objectForKey:self.currentThought.objectId];
     
+    if ([savedVote isEqualToString:@"voteDown"]) {
+     
+        self.scoreDownButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"thumb down inverse ACTIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreDown)];
+        
+        self.scoreUpButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Thumb up WHITE PASSIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreUp)];
+        self.scoreDownButton.enabled = NO;
+        
+    } else if ([savedVote isEqualToString:@"voteUp"]){
+        
+        self.scoreDownButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"thumb down WHITE PASSIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreDown)];
+        
+        self.scoreUpButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Thumb up inverse ACTIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreUp)];
+        self.scoreUpButton.enabled = NO;
+
+        
+    } else {
+        
+        self.scoreDownButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"thumb down WHITE PASSIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreDown)];
+        
+        self.scoreUpButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Thumb up WHITE PASSIVE"] style:UIBarButtonItemStylePlain target:self action:@selector(scoreUp)];
+    }
+      
+    
+//    // ScoreLabel programmatically
+//    UILabel *scoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
+//    NSNumber *score = [self.currentThought objectForKey:@"score"];
+//    scoreLabel.text = [NSString stringWithFormat:@" %@", score];
+//    scoreLabel.numberOfLines = 1;
+//    scoreLabel.textColor = [UIColor whiteColor];
+//        
+//    UIBarButtonItem *displayScoreButton = [[UIBarButtonItem alloc] initWithCustomView:scoreLabel];
+
     self.toolbarItems = [NSArray arrayWithObjects: self.scoreDownButton, space, self.scoreUpButton, nil];
-    
-//    // Toolbar
-//    [self.navigationController setToolbarHidden:NO];
-//    [self.navigationController setToolbarItems:self.toolbarItems];
-//    
-//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//    NSString *savedVote = [defaults objectForKey:self.currentThought.objectId];
-//    
-//    
-//    // If the user has posted an up or down vote
-//    if ([savedVote isEqualToString:@"voteDown"]) {
-//        
-//        self.scoreDownButton.enabled = NO;
-//        self.scoreUpButton.enabled = YES;
-//        
-//     
-//    } else if ([savedVote isEqualToString:@"voteUp"]) {
-//        
-//        self.scoreUpButton.enabled = NO;
-//        self.scoreDownButton.enabled = YES;
-//        
-//    } else {
-//        
-//    }
-    
     
     // If user has posted the thought himself, he cannot upvote the thought
     if ([[self.currentThought objectForKey:@"userName"] isEqualToString:currentUser.username]) {
@@ -125,26 +133,7 @@
 }
 
 
-#pragma mark - score up score down user is wrong
-
-- (void)userIsWrong {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSString *savedVote = [defaults objectForKey:self.currentThought.objectId];
-    
-    // If the user has posted an up or down vote
-    if ([savedVote isEqualToString:@"voteDown"]) {
-        
-        self.scoreDownButton.enabled = NO;
-        self.scoreUpButton.enabled = YES;
-        
-    } else if ([savedVote isEqualToString:@"voteUp"])
-        
-        self.scoreDownButton.enabled = YES;
-        self.scoreUpButton.enabled = NO;
-
-}
-
+#pragma mark - score up score down
 
 - (void)scoreDown {
     
@@ -153,10 +142,34 @@
         
         NSString *user = [self.currentThought objectForKey:@"userName"];
         
-        // If user has not posted the thought himself, he can upvote the thought
+        // If user has not posted the thought himself, he cannot downvote the thought
         if (![[self.currentThought objectForKey:@"userName"] isEqualToString:currentUser.username]) {
-      
             
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            NSString *savedVote = [defaults objectForKey:self.currentThought.objectId];
+            
+            // Voting down after having voted up
+            if ([savedVote isEqualToString:@"voteUp"]) {
+                
+                PFQuery *query = [PFQuery queryWithClassName:@"TotalScore"];
+                [query whereKey:@"userName" equalTo:user];
+                [query getFirstObjectInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
+                    
+                    if (object) {
+                        
+                        PFObject *userObject = object;
+                        [userObject incrementKey:@"totalScore" byAmount:@-2];
+                        [userObject saveInBackground];
+                    }
+                }];
+                
+                [self.currentThought incrementKey:@"score" byAmount:@ -2];
+                [self.currentThought saveInBackground];
+                self.scoreUpButton.enabled = YES;
+                
+            } else {
+            
+            // Voting down for the first time
             PFQuery *query = [PFQuery queryWithClassName:@"TotalScore"];
             [query whereKey:@"userName" equalTo:user];
             [query getFirstObjectInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
@@ -170,28 +183,26 @@
             }];
             
             [self.currentThought incrementKey:@"score" byAmount:@ -1];
-            [self.currentThought saveInBackground];
-            self.hasUserVotedDown = YES;
+            [self.currentThought saveInBackground];            
+            }
             
             self.scoreNumber = [self.currentThought objectForKey:@"score"];
             self.selectedThoughtScore.text = [NSString stringWithFormat:@" %@", self.scoreNumber];
             
-        }
-        if (self.hasUserVotedDown) {
-            
+            self.scoreDownButton.image = [UIImage imageNamed:@"thumb down inverse ACTIVE"];
+            self.scoreUpButton.image = [UIImage imageNamed:@"Thumb up WHITE PASSIVE"];
             self.scoreDownButton.enabled = NO;
-            self.scoreUpButton.enabled = YES;
             
             NSString *voteDown = @"voteDown";
             
-            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
             [defaults setObject:voteDown forKey:self.currentThought.objectId];
             [defaults synchronize];
             
-             }
-    }
+        }
+        }
     
     else {
+        // Not logged in
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Not logged in!"
                                                                        message:@"You need to be logged in to vote up or down!"
                                                                 preferredStyle:UIAlertControllerStyleAlert];
@@ -211,9 +222,15 @@
         
         NSString *user = [self.currentThought objectForKey:@"userName"];
         
+        // If user has not posted the thought himself, he cannot upvote the thought
         if (![[self.currentThought objectForKey:@"userName"] isEqualToString:currentUser.username]) {
-        
             
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            NSString *savedVote = [defaults objectForKey:self.currentThought.objectId];
+            
+            if ([savedVote isEqualToString:@"voteDown"]) {
+
+            // Upvoting after having downvoted
             PFQuery *query = [PFQuery queryWithClassName:@"TotalScore"];
             [query whereKey:@"userName" equalTo:user];
             [query getFirstObjectInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
@@ -221,32 +238,52 @@
                 if (object) {
                     
                     PFObject *userObject = object;
-                    [userObject incrementKey:@"totalScore" byAmount:@1];
+                    [userObject incrementKey:@"totalScore" byAmount:@2];
                     [userObject saveInBackground];
-                                    }
+                    }
             }];
             
-            [self.currentThought incrementKey:@"score" byAmount:@1];
+            [self.currentThought incrementKey:@"score" byAmount:@2];
             [self.currentThought saveInBackground];
-            self.hasUserVotedUp = YES;
+                
+                self.scoreDownButton.enabled = YES;
+                
+            } else {
+                
+                // Upvoting for the first time
+                PFQuery *query = [PFQuery queryWithClassName:@"TotalScore"];
+                [query whereKey:@"userName" equalTo:user];
+                [query getFirstObjectInBackgroundWithBlock:^(PFObject * _Nullable object, NSError * _Nullable error) {
+                    
+                    if (object) {
+                        
+                        PFObject *userObject = object;
+                        [userObject incrementKey:@"totalScore" byAmount:@1];
+                        [userObject saveInBackground];
+                    }
+                }];
+                
+                [self.currentThought incrementKey:@"score" byAmount:@1];
+                [self.currentThought saveInBackground];
+                
+            }
 
             NSNumber *score = [self.currentThought objectForKey:@"score"];
             self.selectedThoughtScore.text = [NSString stringWithFormat:@" %@", score];
-        }
         
-        if (self.hasUserVotedUp) {
+            self.scoreUpButton.image = [UIImage imageNamed:@"Thumb up inverse ACTIVE"];
+            self.scoreDownButton.image = [UIImage imageNamed:@"thumb down WHITE PASSIVE"];
             self.scoreUpButton.enabled = NO;
-            self.scoreDownButton.enabled = YES;
             
             NSString *voteUp = @"voteUp";
             
-            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
             [defaults setObject:voteUp forKey:self.currentThought.objectId];
             [defaults synchronize];
         }
     }
     else {
         
+        // Not logged in
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Not logged in!"
                                                                        message:@"You need to be logged in to vote up or down!"
                                                                 preferredStyle:UIAlertControllerStyleAlert];
@@ -255,9 +292,7 @@
                                                               handler:^(UIAlertAction * action) {}];
         [alert addAction:defaultAction];
         [self presentViewController:alert animated:YES completion:nil];
-        
     }
-    
 }
 
 @end
